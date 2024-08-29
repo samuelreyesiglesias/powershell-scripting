e95f1b5b6b5068ac81dfa05ba1d0acb664ef8db9
samuelreyesiglesias <samuelreyesiglesias@gmail.com>
2024-08-29 15:56:42 -0600

    first commit

---
+++ HolaMundo.ps1
@@ -0,0 +1,9 @@
+$variable ="Hello World!";
+Write-Host "My First programm in PowerShell - $variable";
+
+$secondVariable ="2d Program!";
+Write-Host "My Second programm in PowerShell - Showing `$secondVariable the value is : $secondVariable";
+
+
+$favoriteNumber =999;
+Write-Host "Adding 5 to your favorite number we have : $($favoriteNumber+5)";
