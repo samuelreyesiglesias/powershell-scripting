3553ef180ef0ca4e110336a97ff39d7dde713581
samuelreyesiglesias <samuelreyesiglesias@gmail.com>
2024-08-29 16:04:31 -0600

    We add more examples of Hello World script program, for beginners of PowerShell scripting initiation.

--- HolaMundo.ps1
+++ HolaMundo.ps1
@@ -7,3 +7,14 @@ Write-Host "My Second programm in PowerShell - Showing `$secondVariable the valu
 
 $favoriteNumber =999;
 Write-Host "Adding 5 to your favorite number we have : $($favoriteNumber+5)";
+
+
+#Single Quotes
+$PI=3.14
+Write-Host 'Here is $PI'; #Prints Here is $PI 
+
+#Interpolated variables 
+Write-Host "Here is $PI"; #Prints Here is 3.14
+
+#$() construct interpolate properties of objects
+Write-Host "Here is a `$PI with value 3.14 adding 10 units is: $($PI+10)"; #Prints Here is a $PI the value is 3.14 adding 10 units is 13.14
